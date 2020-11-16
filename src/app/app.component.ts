69c4752ee55a54dcdda5e4f6a0527733ea4292e9
michael.zikes <michael.zikes@intauria.com>
2020-11-16 17:57:39 +0100

    Flight App init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-flight',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'starter';
+export class AppComponent  {
+  title = 'Hello Angular!';
 }
